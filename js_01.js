71520250eedc309443064c44c8d13d6d7dbec4ea
polozkov <SerSerPoloz@gmai.com>
2024-12-24 17:58:16 +0300

    скопировать

--- js_01.js
+++ js_01.js
@@ -5,7 +5,7 @@ var main_input = window.document.getElementById('idInput');
 var main_button_copy = window.document.getElementById('idButtonCopy');
 
 //размеры квадратных клеток (где все символы перевёртыши)
-var cell_size_xy = [64,64];
+var cell_size_xy = [64, 64];
 var LETTERS_31_EQ = "=АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯ";
 
 //очсить  строку от посторонних символов (кроме равно и русских букв, кроме "Ё" и "Ь")
@@ -17,44 +17,13 @@ function f_clear(s) {
   return s_new;
 }
 
-// Async/await method replacing toBlob() callback
-async function getBlobFromCanvas(canvas) {
-  return new Promise((resolve, reject) => {
-    canvas.toBlob((blob) => {
-      if (blob) {
-        resolve(blob);
-      } else {
-        reject(new Error("Canvas toBlob failed"));
-      }
-    });
-  });
-}
-
-async function copyCanvasContentsToClipboard() {
-  if (ClipboardItem.supports("image/png")) {
-    // Copy canvas to blob
-    try {
-      const blob = await getBlobFromCanvas(main_canvas);
-      // Create ClipboardItem with blob and it's type, and add to an array
-      const data = [new ClipboardItem({ [blob.type]: blob })];
-      // Write the data to the clipboard
-      await navigator.clipboard.write(data);
-      console.log("Copied");
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    console.log("image/png is not supported");
-  }
-}
-
 //риссуй русское слово листовертень
 //из книги Калинина "Видение Тайны, издательство Кучково Поле, 2012 год"
 function f_draw_word(russian_word = main_input.value) {
   //только заглавные буквы
   var s = russian_word.toUpperCase();
   //без Ё и мягкого знака (используй Е и Ъ)
-  s = s.replaceAll("Ё","Е").replaceAll("Ь","Ъ");
+  s = s.replaceAll("Ё", "Е").replaceAll("Ь", "Ъ");
   //убери все посторонние символы
   s = f_clear(s);
 
@@ -68,20 +37,20 @@ function f_draw_word(russian_word = main_input.value) {
   //массив, заполненный нулями
   var arr_order = new Array(my_len).fill(0);
   //пары заглавных русских букв
-  var arr_pairs = arr_order.map((value,i) => s_0_1[0][i] + s_0_1[1][i]);
+  var arr_pairs = arr_order.map((value, i) => s_0_1[0][i] + s_0_1[1][i]);
   //пути к файлам (имя файэла - это пара букв, 31*31 файла-картинки уже есть)
-  var arr_paths = arr_pairs.map(value => "pairs/"+value+".jpeg");
+  var arr_paths = arr_pairs.map(value => "pairs/" + value + ".jpeg");
 
   //генерируй размеры картинки в зависимости от длины слова
   main_canvas.width = cell_size_xy[0] * arr_pairs.length;
   main_canvas.height = cell_size_xy[1];
   var main_context = main_canvas.getContext('2d');
-  
+
   //вставляй символ на нужно место на холсте, все символы имеют одинаковый размер
   function f_crop(text_link, i_char_position) {
     var imageObj = new Image();
     //вставляй символ только после загрузки изображения
-    imageObj.onload = function() {
+    imageObj.onload = function () {
       //с какого момента рисовать символ (по оси "Х")
       var X = i_char_position * cell_size_xy[0];
       //параметры для вставки изображения (все клетки-символы имеют одинаковый размер)
@@ -98,6 +67,19 @@ function f_draw_word(russian_word = main_input.value) {
 //нарисуй пример: слово "слово"
 f_draw_word();
 //события изменения текста в строке ввода
-main_input.oninput = (function () {f_draw_word()});
+main_input.oninput = (function () { f_draw_word() });
 
-main_button_copy.onclick = (function () {copyCanvasContentsToClipboard()});
+function f_copy() {
+  // Convert canvas content to a Blob
+  main_canvas.toBlob(function (blob) {
+    // Write Blob to the clipboard
+    navigator.clipboard.write([new ClipboardItem({ 'image/png': blob })])
+      .then(function () {
+        console.log('Canvas image copied to clipboard successfully');
+      })
+      .catch(function (error) {
+        console.error('Unable to copy canvas image to clipboard:', error);
+      });
+  });
+}
+main_button_copy.onclick = (function () { f_copy() });
